f0372da1812db59dccff46eda3f03c9d95b844a2
deanrobertcook <deanrobertcook@gmail.com>
2017-02-07 11:48:11 +0100

    WIP pushing stuff

--- src/main/scala/org/theronin/problems/coursera/Woche2.scala
+++ src/main/scala/org/theronin/problems/coursera/Woche2.scala
@@ -1,21 +1,20 @@
-package org.theronin.problems
+package org.theronin.problems.coursera
 
 import javafx.application.Application
 import javafx.scene.Scene
-import javafx.scene.chart.{LineChart, NumberAxis, ScatterChart, XYChart}
-import javafx.scene.layout.Pane
+import javafx.scene.chart.{LineChart, NumberAxis, XYChart}
 import javafx.stage.Stage
 
 import com.typesafe.scalalogging.LazyLogging
-import org.theronin.scalatools.csv.Column.DoubleColumn
+import org.theronin.scalatools.csv.Column.{DoubleColumn, IntColumn}
 import org.theronin.scalatools.csv.openCSVFromResource
 import org.theronin.scalatools.graph.JavaFXRunner
 
-object Regression extends LazyLogging {
+object Woche2 extends LazyLogging {
 
   import logger._
 
-  def linearRegression(): Unit = {
+  def uebungEins(): Unit = {
 
     val data = openCSVFromResource("coursera-week2/ex1data1.txt").all()
     val population = DoubleColumn(0, data).list.flatten
@@ -34,27 +33,36 @@ object Regression extends LazyLogging {
 
     debug(s"Square errors once: ${squareErrors(0, 0)}")
 
-    val x = population.map(1D -> _)
+    val rows = population.map(Seq(1D, _)) // adds the x0 value (always 1) to the data set
 
     val alpha = 0.01
     val maxIterations = 1500
 
-    def gradientDecent(th0Init: Double = 0, th1Init: Double = 0, i: Int = 0): (Double, Double) = {
-      if (i == maxIterations) (th0Init, th1Init)
-      else {
-        val th0 = th0Init - alpha * (1D / m) * x.zip(profit).map {
-          case ((x0, x1), y) => (th0Init * x0 + th1Init * x1 - y) * x0
-        }.sum
+    val ths = gradientDecent(rows, profit, alpha, maxIterations)
+    debug(s"th0: ${ths(0)}, th1: ${ths(1)}")
+    plotData(population, profit, ths(0), ths(1))
+  }
 
-        val th1 = th1Init - alpha * (1D / m) * x.zip(profit).map {
-          case ((x0, x1), y) => (th0Init * x0 + th1Init * x1 - y) * x1
-        }.sum
-        gradientDecent(th0, th1, i + 1)
+  def gradientDecent(data: Seq[Seq[Double]], ys: Seq[Double], alpha: Double, maxIterations: Int): Seq[Double] = {
+    val m = data.length.toDouble
+    def iterativeStep(thInits: Seq[Double], i: Int = 0): Seq[Double] = {
+
+      if (i % 50 == 0) debug(s"Current ths: $thInits")
+
+      if (i == maxIterations) thInits
+      else {
+        def partialDeriv(j: Int) = {
+          thInits(j) - alpha * (1D / m) * data.zip(ys).map {
+            case (xs, y) => (thInits.zip(xs).map { case (th, x) => th * x }.sum - y) * xs(j)
+          }.sum
+        }
+        iterativeStep(thInits.indices.map(partialDeriv), i + 1)
       }
     }
 
-    val (th0, th1) = gradientDecent()
-    plotData(population, profit, th0, th1)
+    //initialise all thetas to 0
+    iterativeStep(data.head.indices.map(_ => 0D))
+
   }
 
   def plotData(pop: List[Double], profit: List[Double], th0: Double, th1: Double): Unit = {
@@ -102,6 +110,25 @@ object Regression extends LazyLogging {
     }
 
     JavaFXRunner(application).showGraphBlocking()
+  }
+
+  def uebungZwei(): Unit = {
+    val daten = openCSVFromResource("coursera-week2/ex1data2.txt").all()
+
+    val hausGroessen = IntColumn(0, daten).list.flatten
+    val zimmeranzahlen = IntColumn(1, daten).list.flatten
+    val preiss = IntColumn(2, daten).list.flatten
+
+
+    val rows = hausGroessen.zip(zimmeranzahlen).map { case (gr, zi) => Seq(1D, gr.toDouble, zi.toDouble) }
+
+    val alpha = 0.000001
+    val maxIterations = 30000
+
+    val ths = gradientDecent(rows, preiss.map(_.toDouble), alpha, maxIterations)
+
+    debug(s"Ergebnis: p = ${ths(0)} + ${ths(1)}.x1 + ${ths(2)}.x2")
+
   }
 
 
--- src/test/scala/org/theronin/problems/RegressionSpec.scala
+++ src/test/scala/org/theronin/problems/RegressionSpec.scala
@@ -1,11 +1,16 @@
 package org.theronin.problems
 
 import org.scalatest.FeatureSpec
+import org.theronin.problems.coursera.Woche2
 
 class RegressionSpec extends FeatureSpec {
 
   scenario("Run linear regression") {
-    Regression.linearRegression()
+    Woche2.uebungEins()
+  }
+
+  scenario("Lineare Regression mit mehrere unhabaengige Variablen") {
+    Woche2.uebungZwei()
   }
 
 }
